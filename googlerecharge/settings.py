63dddd7c5cbca0ab2c1857b596261e52fc3e7625
mukul203131 <mukul.kumar@shivila.com>
2022-03-28 15:04:28 +0530

    make it better

--- googlerecharge/settings.py
+++ googlerecharge/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'googlerecharge.urls'
 CORS_ORIGIN_ALLOW_ALL = False
 CORS_ORIGIN_WHITELIST = (
   'http://127.0.0.1:8000',
-  'https://googlerecharge.herokuapp.com/',
+  'https://googlerecharge.herokuapp.com',
 )
 TEMPLATES = [
     {
