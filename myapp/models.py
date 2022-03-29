9587bae7cebbffa36391230758ac7762cb5b3ff6
mukul203131 <mukul.kumar@shivila.com>
2022-03-29 19:14:43 +0530

    make it better

--- myapp/models.py
+++ myapp/models.py
@@ -9,6 +9,7 @@ def tns():
 class Googlerecharge(models.Model):
     email = models.EmailField(null=False,blank=False)
     transid = models.CharField(max_length=15,unique=True,default=tns)
+    googleid = models.CharField(max_length=15,unique=True,null=True,blank=True)
     amount = models.BigIntegerField(null=False,blank=False,default=0)
     date = models.DateField(auto_now=True)
     time = models.TimeField(auto_now=True)
